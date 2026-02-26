4e0a1d74f5f8cb6b2e833e557b46e180ec9459ba
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-02-26 17:36:21 -0300

    feat: implements validation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsEmail, IsOptional, IsString } from 'class-validator';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDto extends User {
+  @IsString()
   declare name: string;
+
+  @IsEmail()
+  @IsOptional()
   declare email?: string;
 }
