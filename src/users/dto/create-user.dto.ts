779f8eadbea5ea9770308cf874f4ae2ec6002f54
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-04-14 21:18:51 -0300

    feat: config jwt create-user

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,25 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 import { User } from '../entities/user.entity';
 
 export class CreateUserDto extends User {
+  @IsEmail()
   declare email: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(80)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password must contain at least one uppercase letter, one lowercase letter, and one number or special character',
+  })
+  declare password: string;
+
+  @IsString()
   declare name: string;
 }
