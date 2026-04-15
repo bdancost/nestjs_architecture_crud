2919469ffc3a8723ff045ead9b23eb0b88f6f165
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2026-04-15 18:47:18 -0300

    feat(user): implement files

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,3 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Prisma } from '@prisma/client';
@@ -21,28 +26,28 @@ export class UserService {
       password: undefined,
     };
   }
-}
 
-findAll() {
-  return `This action returns all user`;
-}
+  findAll() {
+    return `This action returns all user`;
+  }
 
-findById(id: number) {
-  return this.prisma.user.findUnique({
-    where: { id },
-  });
-}
+  findById(id: number) {
+    return this.prisma.user.findUnique({
+      where: { id },
+    });
+  }
 
-findByEmail(email: string) {
-  return this.prisma.user.findUnique({
-    where: { email },
-  });
-}
+  findByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
 
-update(id: number, updateUserDto: UpdateUserDto ) {
-  return `This action updates a #${id} user`;
-}
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
 
-remove(id: number) {
-  return `This action removes a #${id} user`;
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
 }
